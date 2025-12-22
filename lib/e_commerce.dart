1100b4c93a381577b8fc7b2bcc14d22b69b595e8
mdrazaulkabir <razaulkabirit@gmail.com>
2025-12-22 21:50:16 +0600

    initially added all file

---
+++ lib/e_commerce.dart
@@ -0,0 +1,12 @@
+import 'package:e_commerce_app/view/home_page.dart';
+import 'package:flutter/material.dart';
+class ECommerce extends StatelessWidget {
+  const ECommerce({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:e_commerce_app/e_commerce.dart';
+import 'package:flutter/cupertino.dart';
+
+void main(){
+  runApp(const ECommerce());
+}
